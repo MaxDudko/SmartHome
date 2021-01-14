6a0815e673c7e78f0c4fedb93ef7c2884c761029
Maksym Dudko <mdudko@wemyit.com>
2021-01-14 18:14:13 +0200

    fix auth

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -3,76 +3,69 @@ import User from "../models/User";
 import passport from "passport";
 
 class AuthController {
-    private static validate(reqData: { [key: string]: string }, key: string) {
-        if(!reqData[key]) {
-            return false;
-        }
-    };
-
-    private static resError(key: string, res: Response) {
-        res.status(422).json({
-            errors: {
-                [key]: 'is Required',
-            }
-        })
-    };
 
     public register(req: Request, res: Response) {
         const user = req.body;
 
-        !AuthController.validate(user, "email") && AuthController.resError('email', res);
-        !AuthController.validate(user, "password") && AuthController.resError('password', res);
+        if (user.email && user.password && user.fullName) {
+            const finalUser = new User(user);
 
-        const finalUser = new User(user);
+            finalUser.setPassword(user.password);
 
-        finalUser.setPassword(user.password);
-
-        return User.create(finalUser.getAttributes())
-            .then(() => res.json({ user: finalUser.toAuthJSON() }))
-            .catch(error => res.status(400).send(error))
+            return User.create(finalUser.getAttributes())
+                .then(() => res.json({ user: finalUser.toAuthJSON() }))
+                .catch(error => res.status(400).send(error))
+        } else {
+            res.status(422).send("All fields are Required")
+        }
     }
 
     public async login(req: Request, res: Response, next:NextFunction) {
         const user = req.body;
 
-        !AuthController.validate(user, "email") && AuthController.resError('email', res);
-        !AuthController.validate(user, "password") && AuthController.resError('password', res);
-        !AuthController.validate(user, "fullName") && AuthController.resError('fullName', res);
+        if (user.email && user.password) {
+            return passport.authenticate('local', { session: false }, (err, passportUser) => {
+                if(err) {
+                    return next(err);
+                }
 
-        return passport.authenticate('local', { session: false }, (err, passportUser) => {
-            if(err) {
-                return next(err);
-            }
+                if(passportUser) {
+                    const user = passportUser;
+                    user.token = passportUser.generateJWT();
 
-            if(passportUser) {
-                const user = passportUser;
-                user.token = passportUser.generateJWT();
+                    return res.json({ user: user.toAuthJSON() });
+                }
 
-                return res.json({ user: user.toAuthJSON() });
-            }
-
-            return res.status(400).json({
-                errors: {
-                    password: 'is Required',
-                },
-            });
-        })(req, res, next);
+                return res.status(400).json({
+                    errors: {
+                        password: 'is Required',
+                    },
+                });
+            })(req, res, next);
+        } else {
+            res.status(422).send("All fields are Required")
+        }
     }
 
     public async changePassword(req: Request, res: Response) {
 
     }
 
-    public profile(req: Request, res: Response): {} {
-        const email = req.body.email;
-        return User.findOne({where: {email: email}})
-            .then((user) => {
-                if(!user) {
-                    return res.sendStatus(400);
-                }
+    public profile(req: Request, res: Response) {
+        // @ts-ignore
+        const email = req.payload.email;
+        if (email) {
+            return User.findOne({where: {email: email}})
+                .then((user) => {
+                    if(!user) {
+                        return res.sendStatus(400);
+                    }
 
-                return res.json({ user: user.toAuthJSON() });
-            });
+                    return res.json({ user: user.toAuthJSON() });
+                });
+        } else {
+            res.status(422).send("Authentication if Failed")
+        }
     }
 }
 
--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,27 +1,27 @@
 import jwt from "express-jwt";
 import { Request } from "express";
 
-const getTokenFromHeader = (req: Request): string | null => {
-    const { headers: {authorization} } = req;
+const getToken = (req: Request): string | null => {
+    const { body: {token} } = req;
 
-    if(authorization && authorization.split(' ')[0] === 'Token') {
-        return authorization.split(' ')[1];
+    if(token) {
+        return token;
     }
     return null;
 };
 
 const auth = {
     required: jwt({
-        algorithms: [],
+        algorithms: ['HS256'],
         secret: 'secret',
         userProperty: 'payload',
-        getToken: getTokenFromHeader
+        getToken: getToken
     }),
     optional: jwt({
-        algorithms: [],
+        algorithms: ['HS256'],
         secret: 'secret',
         userProperty: 'payload',
-        getToken: getTokenFromHeader,
+        getToken: getToken,
         credentialsRequired: false
     }),
 };
