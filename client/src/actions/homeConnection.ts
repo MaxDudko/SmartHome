26688f3889d87c0ee5333907d3bf9ea0762db4e5
Maksym Dudko <mdudko@wemyit.com>
2021-01-20 17:51:57 +0200

    start work on home connect functionality(Client)

---
+++ client/src/actions/homeConnection.ts
@@ -0,0 +1,44 @@
+export const getHomeListAction = (userId) => {
+    return {
+        type: 'GET_HOME_LIST',
+        payload: {
+            userId: userId
+        }
+    }
+};
+
+export const selectHomeAction = (userId, homeId, key) => {
+    return {
+        type: 'SELECT_HOME',
+        payload: {
+            userId: userId,
+            homeId: homeId,
+            key: key
+        }
+    }
+};
+
+export const createHomeAction = (userId, homeName, homeAddress, role, key) => {
+    return {
+        type: 'CREATE_HOME',
+        payload: {
+            userId: userId,
+            homeName: homeName,
+            homeAddress: homeAddress,
+            role: role,
+            key: key
+        }
+    }
+};
+
+export const joinHomeAction = (userId, homeId, role, key) => {
+    return {
+        type: 'JOIN_HOME',
+        payload: {
+            userId: userId,
+            homeId: homeId,
+            role: role,
+            key: key
+        }
+    }
+};
