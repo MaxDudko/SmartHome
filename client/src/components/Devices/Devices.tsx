17f6d1c77fc374e1b0d776a6074ebdb6655aaa8a
Maksym Dudko <mdudko@wemyit.com>
2021-02-08 17:26:34 +0200

    work on mobile view

--- client/src/components/Devices/Devices.tsx
+++ client/src/components/Devices/Devices.tsx
@@ -14,6 +14,11 @@ interface Props {
 const Devices: React.FC<Props> = props =>  {
     const {homeId, openAddDeviceModalAction, devices, lockToggleAction} = props;
 
+    const formatDate = (date: string) => {
+        const newDate = new Date(date);
+        return ((newDate.getMonth() > 8) ? (newDate.getMonth() + 1) : ('0' + (newDate.getMonth() + 1))) + '/' + ((newDate.getDate() > 9) ? newDate.getDate() : ('0' + newDate.getDate())) + '/' + newDate.getFullYear()
+    };
+
     return (
         <div className={styles.Devices + " col s12 l9"}>
             <h4>Devices</h4>
@@ -69,7 +74,7 @@ const Devices: React.FC<Props> = props =>  {
                                         }
                                     </span>
                                 </td>
-                                <td>{device.updatedAt.toLocaleString()}</td>
+                                <td>{formatDate(device.updatedAt)}</td>
                                 <td>
                                     <Icon>more_vert</Icon>
                                 </td>
--- client/src/components/TopNavbar/TopNavbar.tsx
+++ client/src/components/TopNavbar/TopNavbar.tsx
@@ -36,54 +36,71 @@ const TopNavbar: React.FC<Props> = props =>  {
                 preventScrolling: true
             }}
         >
-            <NavItem href="">
-                <i className="material-icons left">toc</i>
-                <div className={styles.box + " progress"} style={{background: '#20293c'}}>
-                    <div className="determinate"
-                         style={{width: '70%', background: '#1f8efa'}}
-                    />
-                </div>
-            </NavItem>
-            <NavItem href="">
-                <i className="material-icons right">help</i>
-            </NavItem>
-            <NavItem href="">
-                <i className="material-icons right">email</i>
-            </NavItem>
-            <NavItem href="">
-                <i className="material-icons right">notifications</i>
-            </NavItem>
-            <Dropdown
-                id="Dropdown_6"
-                options={{
-                    alignment: 'left',
-                    autoTrigger: true,
-                    closeOnClick: true,
-                    constrainWidth: true,
-                    container: null,
-                    coverTrigger: true,
-                    hover: false,
-                    inDuration: 150,
-                    onCloseEnd: null,
-                    onCloseStart: null,
-                    onOpenEnd: null,
-                    onOpenStart: null,
-                    outDuration: 250
-                }}
-                trigger={<a href="#">Username{' '}<Icon right>arrow_drop_down</Icon></a>}
-            >
-                <a href="#" onClick={() => selectAnotherHomeAction()}>
-                    Switch Home
-                </a>
-                <Divider />
-                <a href="#" onClick={() => logoutUserAction()}>
-                    Logout
-                    <Icon right>exit_to_app</Icon>
-                </a>
-            </Dropdown>
-            <NavItem className="hide-on-med-and-up">
+            <div className="hide-on-med-and-up">
                 <Sidebar/>
-            </NavItem>
+            </div>
+            <div className="navItems" style={{display: "flex"}}>
+                <NavItem href="" style={{width: "100%", display: "flex", alignItems: "center"}}>
+                    <i className="material-icons left">toc</i>
+                    <div className={styles.box + " progress"} style={{background: '#20293c', marginTop: 0}}>
+                        <div className="determinate"
+                             style={{width: '70%', background: '#1f8efa'}}
+                        />
+                    </div>
+                </NavItem>
+                <div className={styles.items} style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    width: "100%"
+                }}>
+                    <NavItem href="">
+                        <i className="material-icons right">help</i>
+                    </NavItem>
+                    <NavItem href="">
+                        <i className="material-icons right">email</i>
+                    </NavItem>
+                    <NavItem href="">
+                        <i className="material-icons right">notifications</i>
+                    </NavItem>
+                    <Dropdown
+                        id="Dropdown_6"
+                        options={{
+                            alignment: 'left',
+                            autoTrigger: true,
+                            closeOnClick: true,
+                            constrainWidth: true,
+                            container: null,
+                            coverTrigger: true,
+                            hover: false,
+                            inDuration: 150,
+                            onCloseEnd: null,
+                            onCloseStart: null,
+                            onOpenEnd: null,
+                            onOpenStart: null,
+                            outDuration: 250
+                        }}
+                        trigger={<a href="#"
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center"
+                                    }}
+                        >
+                            <span>Username{' '}</span>
+                            <Icon right>arrow_drop_down</Icon>
+                        </a>}
+                    >
+                        <a href="#" onClick={() => selectAnotherHomeAction()}>
+                            Switch Home
+                        </a>
+                        <Divider />
+                        <a href="#" onClick={() => logoutUserAction()}>
+                            Logout
+                            <Icon right>exit_to_app</Icon>
+                        </a>
+                    </Dropdown>
+                </div>
+            </div>
         </Navbar>
     );
 }
