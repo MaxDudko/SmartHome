176cba4015ba7de6eeea982612c6aa7bd6e091be
Maksym Dudko <mdudko@wemyit.com>
2021-02-10 17:27:14 +0200

    fix endpoint

--- client/src/services/devicesServices.ts
+++ client/src/services/devicesServices.ts
@@ -1,14 +1,15 @@
 import axios from 'axios'
+const SMART_APP_ENDPOINT = '/smart-api'
 
 export const getDevicesService = (request) => {
-  const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/smart-api/get-devices`
+  const API_ENDPOINT = `${process.env.REACT_APP_API_URL}${SMART_APP_ENDPOINT}/get-devices`
   const data = request.payload
 
   return axios.post(API_ENDPOINT, data)
 }
 
 export const lockToggleService = async (request) => {
-  const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/smart-api/lock-toggle`
+  const API_ENDPOINT = `${process.env.REACT_APP_API_URL}${SMART_APP_ENDPOINT}/lock-toggle`
   const data = request.payload
 
   await axios.post(API_ENDPOINT, data)
