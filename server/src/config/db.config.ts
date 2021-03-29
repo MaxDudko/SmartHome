31dc755fd92009360d30caf2566afb8a5fba3a86
Maksym Dudko <mdudko@wemyit.com>
2021-03-29 13:57:03 +0300

    fix

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -10,7 +10,7 @@ export const sequelize = new Sequelize(
   (process.env.DB_USER as string) || 'admin',
   (process.env.DB_PASSWORD as string) || 'admin',
   {
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: process.env.DB_HOST || 'db',
     dialect: 'postgres',
     logging: process.env.NODE_ENV !== 'test',
     pool: {
