f4bedcc080ae00114e0eed66ca0b8db1bb55f887
Maksym Dudko <mdudko@wemyit.com>
2021-04-22 18:39:13 +0300

    Work on line chart

--- client/src/components/Analytics/LineChart.tsx
+++ client/src/components/Analytics/LineChart.tsx
@@ -1,10 +1,39 @@
 import * as d3 from 'd3'
 import { select } from 'd3-selection'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../Analytics/Analytics.module.scss'
+
+const Tooltip = ({ height, x }) => {
+  return (
+    <g>
+      <rect
+        width="66"
+        height={height - 20}
+        x={x - 30}
+        transform={`translate(-5 -${height - 70})`}
+        rx="4"
+        fill="#2b374d"
+      />
+      <rect
+        x={x - 30}
+        width="66"
+        height="30"
+        transform={`translate(-5 -${height - 70})`}
+        fill="#1F8EFA"
+      />
+      <text x={x - 30} transform={`translate(26 -${height - 90})`} fontSize="20px" fill="#fff">
+        1,458
+      </text>
+      <line x1={x} x2={x} y2={-height + 100} fill="#1F8EFA" stroke="#1F8EFA" strokeWidth="4" />
+      <circle cx={x} cy={0} r="6" fill="#1F8EFA" />
+    </g>
+  )
+}
 
 const LinerChart = (props) => {
   const { width, height, data, colors } = props
+  const [hoveredTick, setHoveredTick] = useState('')
+  const [xTooltip, setXTooltip] = useState(0)
 
   const xScale = d3
     .scaleTime()
@@ -34,61 +63,84 @@ const LinerChart = (props) => {
     .x((d) => xScale(d.period))
     .y((d) => yScale(+d.value))
 
+  const area = d3
+    .area()
+    .x((d) => xScale(d.period))
+    .y0(height - 50)
+    .y1((d) => yScale(+d.value))
+
   return (
-    <svg height={height} width={width} className={styles.lineChart}>
-      <g
-        className="x-axis"
-        ref={(node) => select(node).call(xAxis)}
-        transform={`translate(50 ${height - 50})`}
-        strokeDasharray="5"
-      />
-      <g
-        className="y-axis"
-        ref={(node) => select(node).call(yAxis)}
-        transform={`translate(50 0)`}
-      />
-      {data.map((d, i) => {
-        return (
-          <>
-            {/*<defs>*/}
-            {/*  <linearGradient id="gradient" gradientTransform="rotate(90)">*/}
-            {/*    <stop offset="50%" stopColor={colors[i]} />*/}
-            {/*    <stop offset="0%" stopColor="transparent" />*/}
-            {/*  </linearGradient>*/}
-            {/*</defs>*/}
+    <>
+      <svg height={height} width={width} className={styles.lineChart}>
+        <g
+          className="x-axis"
+          ref={(node) => select(node).call(xAxis)}
+          transform={`translate(50 ${height - 50})`}
+          strokeDasharray="5"
+          children={<Tooltip height={height} x={xScale(new Date(2020, 6, 1))} />}
+        />
+        <g
+          className="y-axis"
+          ref={(node) => select(node).call(yAxis)}
+          transform={`translate(50 0)`}
+        />
+        {data.map((d, i) => {
+          return (
             <path
               d={line(d)}
               transform={`translate(50 0)`}
               key={i}
-              fill={i === 0 ? colors[i] : 'none'}
+              fill={'none'}
               stroke={colors[i]}
               strokeWidth="2"
-              fillOpacity="0.05"
+              // fillOpacity="0.05"
             />
-          </>
-        )
-      })}
-      {data.map((d, i) => {
-        const color = colors[i]
-        return d.map((d, i) => {
-          if (i > 0) {
-            return (
-              <circle
-                key={i}
-                cx={xScale(d.period)}
-                cy={yScale(+d.value)}
-                r="2"
-                transform={`translate(50 0)`}
-                stroke={color}
-                fill="#2F3B52"
-              />
-            )
-          } else {
-            return null
-          }
-        })
-      })}
-    </svg>
+          )
+        })}
+        <g>
+          <linearGradient id="Gradient1" x1="0" x2="0" y1="0" y2="1">
+            <stop stopColor={colors[0]} offset="0%" />
+            <stop stopColor="#2f3b52" offset="100%" />
+          </linearGradient>
+          <path
+            d={area(data[0])}
+            transform={`translate(50 0)`}
+            fill="url('#Gradient1')"
+            fillOpacity="0.1"
+          />
+        </g>
+        {data.map((d, i) => {
+          const color = colors[i]
+          return d.map((d, i) => {
+            if (i > 0) {
+              return (
+                <circle
+                  key={i}
+                  cx={xScale(d.period)}
+                  cy={yScale(+d.value)}
+                  r="2"
+                  transform={`translate(50 0)`}
+                  stroke={color}
+                  fill="#2F3B52"
+                />
+              )
+            } else {
+              return null
+            }
+          })
+        })}
+        <rect
+          className="rect"
+          width={width - 50}
+          height={height - 50}
+          transform={`translate(50 0)`}
+          fill="transparent"
+          onMouseOver={(e) => console.log(e)}
+          onMouseMove={(e) => console.log(e)}
+          onMouseOut={(e) => console.log(e)}
+        />
+      </svg>
+    </>
   )
 }
 
